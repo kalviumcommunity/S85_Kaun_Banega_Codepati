541857ad553537ea58f0bdf79e6c7016ecde592b
Pushkar <git config --global user.email>
2025-05-29 02:07:29 +0530

    first_entity and tailwind setup

--- backend/model/question-schema.js
+++ backend/model/question-schema.js
@@ -1,11 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const questionSchema = new mongoose.Schema({
+//     question: String,
+//     options: [String],
+//     answer: String,
+// });
+
+// const Question = mongoose.model('Question', questionSchema);
+
+// module.exports = Question;
+
 const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
-    question: String,
-    options: [String],
-    answer: String,
+  question: { type: String, required: true },
+  options: { type: [String], required: true },
+  answer: { type: String, required: true },
+  difficulty: { type: String, default: "medium" },
+  level: { type: Number, default: 1 },
 });
 
-const Question = mongoose.model('Question', questionSchema);
+const Question = mongoose.model("Question", questionSchema);
 
 module.exports = Question;
